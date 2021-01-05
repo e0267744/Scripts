69b3bc591e49cfa58997be7d1f3c42fc404340d8
baymax <36582132+e0267744@users.noreply.github.com>
2021-01-05 13:03:14 +0800

    Add hashthem.sh
    hashthem.sh is a bash script to hash files and get the md5, sha1, sha256 and file type.

---
+++ hashthem.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+
+hashthemall () {
+	md5sum --tag "$1"
+	sha1sum --tag "$1"
+	sha256sum --tag "$1"
+}
+
+for item_to_be_hashed in "$@"
+do
+	if [ ! -f "$item_to_be_hashed" ]
+	then echo "$item_to_be_hashed is not a file, you want to use this script on a file."
+	fi 
+	hashthemall "$item_to_be_hashed"
+	file "$item_to_be_hashed"
+	echo "--------------------------------------------------------------------------------------------"
+done
